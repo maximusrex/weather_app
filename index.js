09b55b79fb0e651708cb419413e6060aa372e1f9
maxsym23 <maxsym@protonmail.com>
2021-10-04 18:01:49 -0400

    port from environment variable

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const Datastore = require("nedb");
 const fetch = require("node-fetch");
 require("dotenv").config();
 
-app.listen(3000, () => console.log("listening at port 3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening at ${port}`));
 
 app.use(express.static("public"));
 //need to parse any incoming data
